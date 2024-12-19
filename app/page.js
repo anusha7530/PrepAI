ed04cfeda2c5f43db3d16f2f3900bffdf7d6fb4e
anusha7530 <agarwalanusha13@gmail.com>
2024-12-19 23:12:11 +0530

    Added responsiveness

--- app/page.js
+++ app/page.js
@@ -6,13 +6,13 @@ export default function Home() {
   return (
     <div className="bg-gray-200">
       <Header />
-      <div className="flex flex-col lg:flex-row gap-5 items-center">
-        <img src={"/bgImg.webp"} alt="PrepAI" className="h-lvh w-1/2" />
+      <div className="flex flex-col lg:flex-row gap-5 items-center p-5">
+        <img src={"/bgImg.webp"} alt="PrepAI" className="lg:h-lvh w-1/2" />
         <div className="flex flex-col items-center gap-10">
-          <h1 className="text-4xl font-bold text-center">
+          <h1 className="sm:text-3xl md:text-4xl font-bold text-center">
             Welcome to PrepAI - Prepare youself for Mock Interviews
           </h1>
-          <p className="text-center">
+          <p className="sm:text-lg text-center">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
