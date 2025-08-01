9ec662c517ca5e3d9c9cc6fa521eba6d84d903a3
anusha7530 <agarwalanusha13@gmail.com>
2025-08-01 13:27:05 +0530

    signup page fix

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -32,7 +32,7 @@ export default function Page() {
             </h2>
 
             <p className="mt-4 leading-relaxed text-white/90">
-              Prepare yourself for interviews by PrepAI Mock Interviews.
+              Prepare yourself for interviews by PrepAI.
             </p>
           </div>
         </section>
@@ -59,12 +59,11 @@ export default function Page() {
               </a>
 
               <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                Welcome to Squid 🦑
+                Welcome to Prep AI 🦑
               </h1>
 
               <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+                Prepare yourself for interviews by PrepAI.
               </p>
             </div>
 
