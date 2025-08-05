4e60a097e0de11beaf06ffc30615497f38d419fe
anusha7530 <agarwalanusha13@gmail.com>
2025-08-05 20:27:25 +0530

    formatted headings

--- app/dashboard/mocks/page.jsx
+++ app/dashboard/mocks/page.jsx
@@ -5,7 +5,7 @@ import InterviewList from "./_components/InterviewList";
 const Mocks = () => {
   return (
     <div className="p-10 ">
-      <h2 className="font-bold text-2xl text-center text-primary">Mock Interviews</h2>
+      <h2 className="font-bold gradient-title text-5xl md:text-6xl text-primary">Mock Interviews</h2>
       <h2 className="text-gray-500">Create and Start your AI Mock Interview</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview />
--- app/dashboard/quiz/page.jsx
+++ app/dashboard/quiz/page.jsx
@@ -7,7 +7,7 @@ import QuizList from "./_components/QuizList"
 const Quiz = () => {
   return (
     <div className="p-10 ">
-          <h2 className="font-bold text-2xl text-center text-primary">Quizzes</h2>
+          <h2 className="font-bold gradient-title text-5xl md:text-6xl text-primary">Quizzes</h2>
           <h2 className="text-gray-500">Create and Start your AI Generated Quizzes</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 my-5">
             <AddNewQuiz />
